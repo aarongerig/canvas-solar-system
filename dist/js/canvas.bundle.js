34788100407502f9c24060a306ad3112970e081e
Aaron Gerig <a.gerig@w-vision.ch>
2018-03-28 18:05:13 +0200

    WIP

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -75,6 +75,16 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _planetData = __webpack_require__(1);
+
+var _planetData2 = _interopRequireDefault(_planetData);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 // Initial Setup
@@ -90,7 +100,13 @@ var mouse = {
   y: innerHeight / 2
 };
 
-var colors = ['#00bdff', '#4d39ce', '#088eff'];
+var center = {
+  x: innerWidth / 2,
+  y: innerHeight / 2
+};
+
+var scaleFactor = 1.1;
+var AE = 149597870700;
 
 // Event Listeners
 addEventListener('mousemove', function (event) {
@@ -106,99 +122,211 @@ addEventListener('resize', function () {
 });
 
 // Utility Functions
-var randomIntFromRange = function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-var randomColor = function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)];
+var randomFromRange = function randomFromRange(min, max) {
+  return Math.random() * (max - min + 1) + min;
 };
 
 // Objects
 
-var Particle = function () {
-  function Particle(x, y, radius, color) {
-    _classCallCheck(this, Particle);
+var Luminary = function Luminary(radius, distance, velocity) {
+  var color = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : '#9d9d9c';
 
-    this.x = x;
-    this.y = y;
+  _classCallCheck(this, Luminary);
+
+  this.x = center.x;
+  this.y = center.y;
+  this.radius = radius * 0.001;
+  this.distance = distance * 100;
+  this.velocity = velocity * 0.001;
+  this.color = color;
+  this.radians = Math.random() * Math.PI * 2;
+};
+
+var Star = function () {
+  function Star(label, radius, color) {
+    _classCallCheck(this, Star);
+
+    this.x = center.x;
+    this.y = center.y;
+    this.label = label;
     this.radius = radius;
     this.color = color;
-    this.radians = Math.random() * Math.PI * 2;
-    this.velocity = 0.05;
-    this.distanceFromCenter = randomIntFromRange(50, 120);
-    this.lastMouse = {
-      x: x,
-      y: y
-    };
   }
 
-  _createClass(Particle, [{
+  _createClass(Star, [{
     key: 'draw',
-    value: function draw(lastPoint) {
+    value: function draw() {
       c.beginPath();
-      c.strokeStyle = this.color;
-      c.lineWidth = this.radius;
-      c.moveTo(lastPoint.x, lastPoint.y);
-      c.lineTo(this.x, this.y);
+      c.arc(this.x, this.y, this.radius * 0.01, 0, Math.PI * 2, false);
+      c.fillStyle = this.color;
+      c.fill();
+      c.closePath();
+    }
+  }]);
+
+  return Star;
+}();
+
+var Planet = function (_Luminary) {
+  _inherits(Planet, _Luminary);
+
+  function Planet(label, radius, distance, velocity, color) {
+    _classCallCheck(this, Planet);
+
+    var _this = _possibleConstructorReturn(this, (Planet.__proto__ || Object.getPrototypeOf(Planet)).call(this, radius, distance, velocity, color));
+
+    _this.label = label;
+    return _this;
+  }
+
+  _createClass(Planet, [{
+    key: 'draw',
+    value: function draw() {
+      // Orbit
+      c.beginPath();
+      c.arc(center.x, center.y, this.distance, 0, Math.PI * 2, false);
+      c.strokeStyle = 'rgba(255, 255, 255, 0.25)';
       c.stroke();
+      c.closePath();
+
+      // Planet
+      c.beginPath();
+      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      c.fillStyle = this.color;
+      c.fill();
       c.closePath();
     }
   }, {
     key: 'update',
     value: function update() {
-      var lastPoint = {
-        x: this.x,
-        y: this.y
-      };
-
-      // Move points over time
+      // Move planets over time
       this.radians += this.velocity;
 
-      // Drag effect
-      this.lastMouse.x += (mouse.x - this.lastMouse.x) * 0.05;
-      this.lastMouse.y += (mouse.y - this.lastMouse.y) * 0.05;
-
       // Circular motion
-      this.x = this.lastMouse.x + Math.cos(this.radians) * this.distanceFromCenter;
-      this.y = this.lastMouse.y + Math.sin(this.radians) * this.distanceFromCenter;
+      this.x = center.x + Math.cos(this.radians) * this.distance;
+      this.y = center.y + Math.sin(this.radians) * this.distance;
 
-      this.draw(lastPoint);
+      this.draw();
     }
   }]);
 
-  return Particle;
-}();
+  return Planet;
+}(Luminary);
+
+var Asteroid = function (_Luminary2) {
+  _inherits(Asteroid, _Luminary2);
+
+  function Asteroid(radius, distance, velocity) {
+    _classCallCheck(this, Asteroid);
+
+    return _possibleConstructorReturn(this, (Asteroid.__proto__ || Object.getPrototypeOf(Asteroid)).call(this, radius, distance, velocity));
+  }
+
+  _createClass(Asteroid, [{
+    key: 'draw',
+    value: function draw() {
+      c.beginPath();
+      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      c.fillStyle = this.color;
+      c.fill();
+      c.closePath();
+    }
+  }, {
+    key: 'update',
+    value: function update() {
+      // Move planets over time
+      this.radians += this.velocity;
+
+      // Circular motion
+      this.x = center.x + Math.cos(this.radians) * this.distance;
+      this.y = center.y + Math.sin(this.radians) * this.distance;
+
+      this.draw();
+    }
+  }]);
+
+  return Asteroid;
+}(Luminary);
 
 // Implementation
 
 
-var particles = void 0;
+var sun = void 0;
+var planets = void 0;
+var asteroids = void 0;
 
 var init = function init() {
-  particles = [];
+  planets = [];
+  asteroids = [];
 
-  for (var i = 0; i < 50; i++) {
-    var radius = randomIntFromRange(2, 4);
-    var color = randomColor(colors);
+  sun = new Star('Sun', 1000, '#ffe64a');
 
-    particles.push(new Particle(canvas.width / 2, canvas.height / 2, radius, color));
+  _planetData2.default.forEach(function (data) {
+    planets.push(new Planet(data.label, data.props.radius, data.props.distance, data.props.velocity, data.props.color));
+  });
+
+  for (var i = 0; i < 1000; i++) {
+    var radius = randomFromRange(100, 800);
+    var distance = randomFromRange(2, 3.4);
+    var velocity = randomFromRange(0.2, 0.6);
+
+    asteroids.push(new Asteroid(radius, distance, velocity));
   }
 };
+
+// const redraw = () => {
+//   // Clear the entire canvas
+//   c.save();
+//   c.setTransform(1, 0, 0, 1, 0, 0);
+//   c.clearRect(0, 0, canvas.width, canvas.height);
+//   c.restore();
+// };
+//
+// const zoom = (clicks) => {
+//   const factor = Math.pow(scaleFactor, clicks);
+//
+//   c.translate(center.x, center.y);
+//   c.scale(factor, factor);
+//   c.translate(-center.x, -center.y);
+//
+//   redraw();
+// };
+//
+// const handleScroll = (e) => {
+//   const delta = e.wheelDelta ? e.wheelDelta / 40 : e.detail ? -e.detail : 0;
+//   if (delta) zoom(delta);
+//   return e.preventDefault() && false;
+// };
+//
+// canvas.addEventListener('DOMMouseScroll', handleScroll, false);
+// canvas.addEventListener('mousewheel', handleScroll, false);
 
 // Animation Loop
 var animate = function animate() {
   requestAnimationFrame(animate);
-  c.fillStyle = 'rgba(255, 255, 255, 0.05)';
+  c.fillStyle = 'black';
   c.fillRect(0, 0, canvas.width, canvas.height);
 
-  particles.forEach(function (particle) {
-    particle.update();
+  sun.draw();
+
+  planets.forEach(function (planet) {
+    planet.update();
+  });
+
+  asteroids.forEach(function (asteroid) {
+    asteroid.update();
   });
 };
 
 init();
+// redraw();
 animate();
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+module.exports = [{"label":"Mercury","props":{"radius":2439.764,"distance":0.38709888,"velocity":47.8725,"color":"#8c8b79"}},{"label":"Venus","props":{"radius":6051.59,"distance":0.72333193,"velocity":35.0214,"color":"#b78b5f"}},{"label":"Earth","props":{"radius":6378.15,"distance":1,"velocity":29.7859,"color":"#6fbccf"}},{"label":"Mars","props":{"radius":3397,"distance":1.5236621,"velocity":24.1309,"color":"#b85139"}},{"label":"Jupiter","props":{"radius":71492.68,"distance":5.2033623,"velocity":13.0697,"color":"#8caaa7"}},{"label":"Saturn","props":{"radius":60267.14,"distance":9.537069,"velocity":9.6724,"color":"#d6cab1"}},{"label":"Uranus","props":{"radius":25559,"distance":19.191261,"velocity":6.8352,"color":"#7fada9"}},{"label":"Neptun","props":{"radius":24764,"distance":30.06896,"velocity":5.4778,"color":"#355f8f"}},{"label":"Pluto","props":{"radius":1187,"distance":39.482,"velocity":4.668,"color":"#b17f4a"}}]
 
 /***/ })
 /******/ ]);
--- src/canvas.js
+++ src/canvas.js
@@ -1,3 +1,5 @@
+import planetData from './planet-data.json';
+
 // Initial Setup
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
@@ -9,13 +11,16 @@ canvas.height = innerHeight;
 const mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2
-}
+};
 
-const colors = [
-  '#00bdff',
-  '#4d39ce',
-  '#088eff'
-];
+const center = {
+  x: innerWidth / 2,
+  y: innerHeight / 2
+};
+
+const scaleFactor = 1.1;
+const AE = 149597870700;
+
 
 // Event Listeners
 addEventListener('mousemove', event => {
@@ -31,87 +36,177 @@ addEventListener('resize', () => {
 });
 
 // Utility Functions
-const randomIntFromRange = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-const randomColor = (colors) => {
-  return colors[Math.floor(Math.random() * colors.length)];
+const randomFromRange = (min, max) => {
+  return Math.random() * (max - min + 1) + min;
 };
 
 // Objects
-class Particle {
+class Luminary {
 
-  constructor(x, y, radius, color) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
+  constructor(radius, distance, velocity, color = '#9d9d9c') {
+    this.x = center.x;
+    this.y = center.y;
+    this.radius = radius * 0.001;
+    this.distance = distance * 100;
+    this.velocity = velocity * 0.001;
     this.color = color;
     this.radians = Math.random() * Math.PI * 2;
-    this.velocity = 0.05;
-    this.distanceFromCenter = randomIntFromRange(50, 120);
-    this.lastMouse = {
-      x: x,
-      y: y
-    };
   }
 
-  draw(lastPoint) {
+}
+
+class Star {
+
+  constructor(label, radius, color) {
+    this.x = center.x;
+    this.y = center.y;
+    this.label = label;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw() {
     c.beginPath();
-    c.strokeStyle = this.color;
-    c.lineWidth = this.radius;
-    c.moveTo(lastPoint.x, lastPoint.y);
-    c.lineTo(this.x, this.y);
+    c.arc(this.x, this.y, this.radius * 0.01, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+  }
+
+}
+
+class Planet extends Luminary {
+
+  constructor(label, radius, distance, velocity, color) {
+    super(radius, distance, velocity, color);
+
+    this.label = label;
+  }
+
+  draw() {
+    // Orbit
+    c.beginPath();
+    c.arc(center.x, center.y, this.distance, 0, Math.PI * 2, false);
+    c.strokeStyle = 'rgba(255, 255, 255, 0.25)';
     c.stroke();
+    c.closePath();
+
+    // Planet
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
     c.closePath();
   }
 
   update() {
-    const lastPoint = {
-      x: this.x,
-      y: this.y
-    };
-
-    // Move points over time
+    // Move planets over time
     this.radians += this.velocity;
 
-    // Drag effect
-    this.lastMouse.x += (mouse.x - this.lastMouse.x) * 0.05;
-    this.lastMouse.y += (mouse.y - this.lastMouse.y) * 0.05;
+    // Circular motion
+    this.x = center.x + Math.cos(this.radians) * this.distance;
+    this.y = center.y + Math.sin(this.radians) * this.distance;
+
+    this.draw();
+  }
+
+}
+
+class Asteroid extends Luminary {
+
+  constructor(radius, distance, velocity) {
+    super(radius, distance, velocity);
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+  }
+
+  update() {
+    // Move planets over time
+    this.radians += this.velocity;
 
     // Circular motion
-    this.x = this.lastMouse.x + Math.cos(this.radians) * this.distanceFromCenter;
-    this.y = this.lastMouse.y + Math.sin(this.radians) * this.distanceFromCenter;
+    this.x = center.x + Math.cos(this.radians) * this.distance;
+    this.y = center.y + Math.sin(this.radians) * this.distance;
 
-    this.draw(lastPoint);
+    this.draw();
   }
 
 }
 
 // Implementation
-let particles;
+let sun;
+let planets;
+let asteroids;
 
 const init = () => {
-  particles = [];
+  planets = [];
+  asteroids = [];
 
-  for (let i = 0; i < 50; i++) {
-    const radius = randomIntFromRange(2, 4);
-    const color = randomColor(colors);
+  sun = new Star('Sun', 1000, '#ffe64a');
 
-    particles.push(new Particle(canvas.width / 2, canvas.height / 2, radius, color));
+  planetData.forEach((data) => {
+    planets.push(new Planet(data.label, data.props.radius, data.props.distance, data.props.velocity, data.props.color));
+  });
+
+  for (let i = 0; i < 1000; i++) {
+    const radius = randomFromRange(100, 800);
+    const distance = randomFromRange(2, 3.4);
+    const velocity = randomFromRange(0.2, 0.6);
+
+    asteroids.push(new Asteroid(radius, distance, velocity));
   }
 };
+
+// const redraw = () => {
+//   // Clear the entire canvas
+//   c.save();
+//   c.setTransform(1, 0, 0, 1, 0, 0);
+//   c.clearRect(0, 0, canvas.width, canvas.height);
+//   c.restore();
+// };
+//
+// const zoom = (clicks) => {
+//   const factor = Math.pow(scaleFactor, clicks);
+//
+//   c.translate(center.x, center.y);
+//   c.scale(factor, factor);
+//   c.translate(-center.x, -center.y);
+//
+//   redraw();
+// };
+//
+// const handleScroll = (e) => {
+//   const delta = e.wheelDelta ? e.wheelDelta / 40 : e.detail ? -e.detail : 0;
+//   if (delta) zoom(delta);
+//   return e.preventDefault() && false;
+// };
+//
+// canvas.addEventListener('DOMMouseScroll', handleScroll, false);
+// canvas.addEventListener('mousewheel', handleScroll, false);
 
 // Animation Loop
 const animate = () => {
   requestAnimationFrame(animate);
-  c.fillStyle = 'rgba(255, 255, 255, 0.05)';
+  c.fillStyle = 'black';
   c.fillRect(0, 0, canvas.width, canvas.height);
 
-  particles.forEach((particle) => {
-    particle.update();
+  sun.draw();
+
+  planets.forEach((planet) => {
+    planet.update();
+  });
+
+  asteroids.forEach((asteroid) => {
+    asteroid.update();
   });
 };
 
 init();
+// redraw();
 animate();
